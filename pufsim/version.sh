0835cecdc087f9172642399e9307e6d712783e56
Gregory N. Schmit <schmitgreg@gmail.com>
2018-10-09 00:38:54 -0500

    revise version script to attempt to comply with PEP440

--- pufsim/version.sh
+++ pufsim/version.sh
@@ -1,5 +1,5 @@
 
-GITVER=$(git describe --tags --long --always)
-GITCHANGED=$(git diff-index --quiet HEAD -- || echo '-changed')
+GITVER=$(git describe --tags --long --always | sed 's/v//g' | sed 's/-g.*//g' | sed 's/-/\.dev/g')
+GITCHANGED=$(git diff-index --quiet HEAD -- || echo '+changed')
 
 echo $GITVER$GITCHANGED
