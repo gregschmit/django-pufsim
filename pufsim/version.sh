34e9ec7524378ee433f4bb2bb04e2142b8ea7c36
Gregory N. Schmit <schmitgreg@gmail.com>
2018-10-09 00:47:19 -0500

    revise version to shorten for releases

--- pufsim/version.sh
+++ pufsim/version.sh
@@ -1,5 +1,5 @@
 
-GITVER=$(git describe --tags --long --always | sed 's/v//g' | sed 's/-g.*//g' | sed 's/-/\.dev/g')
+GITVER=$(git describe --tags --always | sed 's/v//g' | sed 's/-g.*//g' | sed 's/-/\.dev/g')
 GITCHANGED=$(git diff-index --quiet HEAD -- || echo '+changed')
 
 echo $GITVER$GITCHANGED
